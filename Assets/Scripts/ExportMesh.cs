de3e31dae2d4bdbaf080d28fda04538dca759047
mdaniels9905 <mdaniels9905@gmail.com>
2023-08-08 10:54:45 -0700

    Export loading bar added

--- Assets/Scripts/ExportMesh.cs
+++ Assets/Scripts/ExportMesh.cs
@@ -2,8 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
+using TMPro;
 using UnityEngine;
 
 public class ExportMesh : MonoBehaviour {
@@ -13,6 +12,9 @@ public class ExportMesh : MonoBehaviour {
 
     [SerializeField]
     private GameObject progressIndicator;
+
+    [SerializeField]
+    private TextMeshPro progressText;
 
     [SerializeField]
     private GameObject sceneObjects;
@@ -58,7 +60,8 @@ public class ExportMesh : MonoBehaviour {
 
     public void StartProcessing () {
         progressIndicator.SetActive( true );
-        progressIndicator.transform.localPosition = Camera.main.transform.localPosition + Camera.main.transform.forward;
+        progressIndicator.transform.localPosition = Camera.main.transform.localPosition + 0.75f * Camera.main.transform.forward;
+        progressText.text = "Final Touches ...";
 
         progressBar.transform.localScale = new( 0, 1, 1 );
         menu.SetActive( false );
@@ -68,35 +71,48 @@ public class ExportMesh : MonoBehaviour {
     }
 
     private IEnumerator ExtrudeMesh () {
-        Mesh innerWall = meshDeformer.DeformedMesh;
-        Mesh mesh = Instantiate( innerWall );
-        yield return null;
-        progressBar.transform.localScale = new( 0.1f, 1, 1 );
-
+        Mesh mesh = meshDeformer.DeformedMesh;
         Vector3[] vertices = mesh.vertices;
         int[] triangles = mesh.triangles;
+
+        progressBar.transform.localScale = new( 0.1f, 1, 1 );
+        progressText.text = "Simplifying mesh ...";
+        yield return StartCoroutine( SimplifyMesh( vertices, triangles, simplifiedMesh => { mesh = simplifiedMesh; } ) );
+        progressBar.transform.localScale = new( 0.3f, 1, 1 );
+
+        progressText.text = "Extruding mesh ...";
 
         Vector3[] newVertices = new Vector3[ mesh.vertices.Length * 2 ];
         int[] newTriangles = new int[ mesh.triangles.Length * 2 ];
 
         Array.Copy( vertices, newVertices, mesh.vertices.Length );
+        //yield return null;
         Array.Copy( triangles, newTriangles, mesh.triangles.Length );
 
-        progressBar.transform.localScale = new( 0.65f, 1, 1 );
-
+        int counter = 0;
         for ( int i = 0; i < mesh.vertices.Length; i++ ) {
-            newVertices[ i + mesh.vertices.Length ] = vertices[ i ] + ( new Vector3( mesh.normals[ i ].x, 0, mesh.normals[ i ].z ) * extrusionDistance );
-            yield return null;
+            //Vector3 normalizedNormal = mesh.normals[ i ].normalized;
+            //newVertices[ i + mesh.vertices.Length ] = vertices[ i ] + ( new Vector3( normalizedNormal.x, 0, normalizedNormal.z ) * extrusionDistance );
+            newVertices[ i + mesh.vertices.Length ] = vertices[ i ] + ( new Vector3( mesh.normals[ i ].x, 0, mesh.normals[ i ].z ).normalized * extrusionDistance );
+            counter++;
+
+            if (counter % 15 == 0)
+                yield return null;
         }
 
+        progressText.text = "Rewinding triangles ...";
+        progressBar.transform.localScale = new( 0.4f, 1, 1 );
+
+        counter = 0;
         for ( int i = 0; i < triangles.Length; i += 3 ) {
             newTriangles[ i + triangles.Length ] = triangles[ i ] + vertices.Length;
             newTriangles[ i + triangles.Length + 1 ] = triangles[ i + 1 ] + vertices.Length;
             newTriangles[ i + triangles.Length + 2 ] = triangles[ i + 2 ] + vertices.Length;
-            yield return null;
-        }
+            counter++;
 
-        progressBar.transform.localScale = new( 0.7f, 1, 1 );
+            if ( counter % 50 == 0 )
+                yield return null;
+        }
 
         mesh.vertices = newVertices;
         mesh.triangles = newTriangles;
@@ -106,67 +122,164 @@ public class ExportMesh : MonoBehaviour {
         GameObject extrudedObject = new();
         extrudedObject.AddComponent<MeshFilter>().mesh = mesh;
 
-        progressBar.transform.localScale = new( 0.75f, 1, 1 );
+        progressBar.transform.localScale = new( 0.5f, 1, 1 );
         Export( extrudedObject );
     }
 
+    private IEnumerator SimplifyMesh ( Vector3[] startingVertices, int[] startingTriangles,  Action<Mesh> callback ) {
+        List<VertexData> vertexInfo = new();
+        int counter = 0;
+
+        //For each unique index in the vertices array, add all occurences of the vertex in the oldIndices list and add that vertex to the vertexInfo array
+        for ( int i = 0; i < startingVertices.Length; i++ ) {
+            Vector3 tempVertex = startingVertices[ i ];
+            bool foundInArray = CheckIfExists( tempVertex, vertexInfo );
+
+            if ( foundInArray )
+                vertexInfo[ index ].OldIndices.Add( i );
+            else
+                vertexInfo.Add( new VertexData( tempVertex, new List<int>() { i }, 0 ) );
+
+            counter++;
+            if ( counter % 10 == 0 )
+                yield return null;
+        }
+
+        //Loop through all triangles and replace all occurences of indices in oldIndices with the new one
+        List<TriangleData> triangleInfo = new();
+        for ( int i = 0; i < startingTriangles.Length; i++ ) {
+            triangleInfo.Add( new TriangleData( startingTriangles[ i ], false ) );
+        }
+
+        progressBar.transform.localScale = new( 0.2f, 1, 1 );
+
+        for ( int i = 0; i < vertexInfo.Count; i++ ) {
+            List<int> oldIndices = vertexInfo[ i ].OldIndices;
+            int newIndex = i;
+
+            for ( int index = 0; index < oldIndices.Count; index++ ) {
+                for ( int j = 0; j < triangleInfo.Count; j++ ) {
+                    if ( oldIndices[ index ] == triangleInfo[ j ].TriangleIndex && triangleInfo[ j ].Changed == false ) {
+                        triangleInfo[ j ].TriangleIndex = newIndex;
+                        triangleInfo[ j ].Changed = true;
+                    }
+                }
+            }
+            
+            yield return null;
+        }
+
+        int[] newTriangles = new int[ triangleInfo.Count ];
+        for ( int i = 0; i < newTriangles.Length; i++ ) {
+            newTriangles[ i ] = triangleInfo[ i ].TriangleIndex;
+        }
+
+        Vector3[] updatedVertices = new Vector3[ vertexInfo.Count ];
+        for ( int i = 0; i < updatedVertices.Length; i++ ) {
+            updatedVertices[ i ] = vertexInfo[ i ].Vertex;
+        }
+
+        Mesh newMesh = new() {
+            vertices = updatedVertices,
+            triangles = newTriangles
+        };
+
+        newMesh.RecalculateBounds();
+        newMesh.RecalculateNormals();
+        callback( newMesh );
+    }
+
+    private bool CheckIfExists ( Vector3 vertex, List<VertexData> vertexInfo ) {
+        for ( int i = 0; i < vertexInfo.Count; i++ ) {
+            if ( vertexInfo[ i ].Vertex == vertex ) {
+                index = i;
+                return true;
+            }
+        }
+        return false;
+    }
+
     private async void Export ( GameObject gameObject ) {
+        progressText.text = "Export started ...";
 
         Mesh mesh = gameObject.GetComponent<MeshFilter>().mesh;
+        progressText.text = "Getting vertices ...";
         Vector3[] vertices = mesh.vertices;
+        progressText.text = "Getting normals ...";
         Vector3[] normals = mesh.normals;
+        progressText.text = "Getting triangles ...";
         int[] triangles = mesh.triangles;
-        progressBar.transform.localScale = new( 0.8f, 1, 1 );
-
-        StringBuilder fileText = new();
-        for ( int i = 0; i < vertices.Length; i++ ) {
-            fileText.AppendLine( $"v {vertices[ i ].x} {vertices[ i ].y} {vertices[ i ].z}\n" );
-        }
-        progressBar.transform.localScale = new( 0.85f, 1, 1 );
-
-        for ( int i = 0; i < normals.Length; i++ ) {
-            fileText.AppendLine( $"vn {normals[ i ].x} {normals[ i ].y} {normals[ i ].z}\n" );
-        }
-        progressBar.transform.localScale = new( 0.9f, 1, 1 );
-
-        for ( int i = 0; i < triangles.Length; i += 3 ) {
-            fileText.AppendLine( $"f {triangles[ i ] + 1}//{triangles[ i ] + 1} {triangles[ i + 1 ] + 1}//{triangles[ i + 1 ] + 1} {triangles[ i + 2 ] + 1}//{triangles[ i + 2 ] + 1}\n" );
-        }
-        progressBar.transform.localScale = new( 0.95f, 1, 1 );
+        progressBar.transform.localScale = new( 0.6f, 1, 1 );
 
         //For exporting on computer
         #if UNITY_EDITOR
-        string filePath = Path.GetFullPath( Path.Combine( Application.dataPath, "Exports", fileName ) );
+            string filePath = Path.GetFullPath( Path.Combine( Application.dataPath, "Exports", fileName ) );
+            progressBar.transform.localScale = new( 0.7f, 1, 1 );
 
             if ( File.Exists( filePath ) )
                 File.Delete( filePath );
 
+            progressText.text = "Writing to file ...";
             try {
                 FileStream stream = new( filePath, FileMode.CreateNew );
                 using ( StreamWriter fileWriter = new( stream ) ) {
-                    await fileWriter.WriteAsync( fileText.ToString() );
-                }
+                    progressText.text = "Exporting vertices (1/3) ...";
+                    for ( int i = 0; i < vertices.Length; i++ ) {
+                        await fileWriter.WriteAsync( $"v {vertices[ i ].x} {vertices[ i ].y} {vertices[ i ].z}\n" );
+                    }
+                    progressBar.transform.localScale = new( 0.8f, 1, 1 );
+
+                    progressText.text = "Exporting normals (2/3) ...";
+                    for ( int i = 0; i < normals.Length; i++ ) {
+                        await fileWriter.WriteAsync( $"vn {normals[ i ].x} {normals[ i ].y} {normals[ i ].z}\n" );
+                    }
+                    progressBar.transform.localScale = new( 0.9f, 1, 1 );
+
+                    progressText.text = "Exporting faces (3/3) ...";
+                    for ( int i = 0; i < triangles.Length; i += 3 ) {
+                        await fileWriter.WriteAsync( $"f {triangles[ i ] + 1}//{triangles[ i ] + 1} {triangles[ i + 1 ] + 1}//{triangles[ i + 1 ] + 1} {triangles[ i + 2 ] + 1}//{triangles[ i + 2 ] + 1}\n" );
+                    }
+                    progressBar.transform.localScale = new( 1, 1, 1 );
+            }
             } catch { }
-            progressBar.transform.localScale = new( 1, 1, 1 );
         #endif
 
         //For exporting on HoloLens
         #if UNITY_WSA && !UNITY_EDITOR
+            progressText.text = "Creating file (1/2)...";
             Windows.Storage.StorageFolder storageFolder = Windows.Storage.KnownFolders.PicturesLibrary;
+            progressText.text = "Creating file (2/2)...";
             Windows.Storage.StorageFile exportedFile = await storageFolder.CreateFileAsync( "ExportedScoliosisBrace.obj", Windows.Storage.CreationCollisionOption.ReplaceExisting );
+            progressBar.transform.localScale = new( 0.7f, 1, 1 );
 
-
+            progressText.text = "Writing to file ...";
             var stream = await exportedFile.OpenAsync( Windows.Storage.FileAccessMode.ReadWrite );
             using ( var outputStream = stream.GetOutputStreamAt( 0 ) ) {
                 using ( var fileWriter = new Windows.Storage.Streams.DataWriter( outputStream ) ) {
-                    fileWriter.WriteString( fileText.ToString() );
-                    
+                
+                    progressText.text = "Exporting vertices (1/3) ...";
+                    for ( int i = 0; i < vertices.Length; i++ ) {
+                        fileWriter.WriteString( $"v {vertices[ i ].x} {vertices[ i ].y} {vertices[ i ].z}\n" );
+                    }
+                    progressBar.transform.localScale = new( 0.8f, 1, 1 );
+
+                    progressText.text = "Exporting normals (2/3) ...";
+                    for ( int i = 0; i < normals.Length; i++ ) {
+                        fileWriter.WriteString( $"vn {normals[ i ].x} {normals[ i ].y} {normals[ i ].z}\n" );
+                    }
+                    progressBar.transform.localScale = new( 0.9f, 1, 1 );
+
+                    progressText.text = "Exporting faces (3/3) ...";
+                    for ( int i = 0; i < triangles.Length; i += 3 ) {
+                        fileWriter.WriteString( $"f {triangles[ i ] + 1}//{triangles[ i ] + 1} {triangles[ i + 1 ] + 1}//{triangles[ i + 1 ] + 1} {triangles[ i + 2 ] + 1}//{triangles[ i + 2 ] + 1}\n" );
+                    }
+                    progressBar.transform.localScale = new( 1, 1, 1 );
+
                     await fileWriter.StoreAsync();
                     await outputStream.FlushAsync();
                 }    
             }
             stream.Dispose();
-            progressBar.transform.localScale = new( 1, 1, 1 );
         #endif
 
         progressIndicator.SetActive( false );
